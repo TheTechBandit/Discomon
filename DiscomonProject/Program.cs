5829116b8d02f533f252e1903c79180160debb6a
GhubMain <datechbandit@gmail.com>
2018-11-26 19:37:19 -0500

    first commit

---
+++ DiscomonProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscomonProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
